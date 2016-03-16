f6ad616b25e2cbd115346cb2d7fdfa1d29d7c4cb
Anthony Lander <anthony.lander@gmail.com>
2016-03-16 08:55:30 -0400

    applies code transformations

--- crackRepeatingKeyXOR.py
+++ crackRepeatingKeyXOR.py
@@ -1,147 +1,157 @@
-import sys
 import pdb
 import base64
+import itertools
 
-MAX_KEYSIZE=40
+MAX_KEYSIZE = 40
 
-def encryptRepeatingXOR( plainText, key ):
+
+def encryptRepeatingXOR(plainText, key):
     keylength = len(key)
-    cipherText = result = [ 0 for i in range( len(plainText)) ]
-    keyPos=0
-    textPos=0
+    cipherText = [0] * len(plainText)
+    keyPos = 0
+    textPos = 0
 
     for b in plainText:
         cipherText[textPos] = ord(plainText[textPos]) ^ ord(key[keyPos])
-        textPos=textPos+1
-        keyPos=keyPos+1
+        textPos += 1
+        keyPos += 1
         if keyPos >= keylength:
-            keyPos=0
+            keyPos = 0
     return cipherText
 
-def alphaRangeScoreText( text ):
-    score = 0
-    textLength = len( text )
-    if ( textLength == 0 ):
-         print "textlength is zero"
-         return 0
-    for b in text:
-        if ((b >= 48) and (b <= 57)) or ((b >= 65) and (b <= 90)) or ((b >= 97) and (b <= 122)) or (b==32):
-            score = score + 1
-    return ( float(score) / float(textLength) )
 
-def decryptXORString( inputString, key ):
-    result = [ 0 for i in range( len(inputString)) ]
-    i=0
+def alphaRangeScoreText(text):
+    score = 0
+    textLength = len(text)
+
+    if textLength == 0:
+        print 'textlength is zero'
+        return 0
+    
+    for b in text:
+        if ((b >= 48) and (b <= 57)) or ((b >= 65) and (b <= 90)) or ((b >= 97) and (b <= 122)) or (b == 32):
+            score += 1
+    return float(score) / float(textLength)
+
+
+def decryptXORString(inputString, key):
+    result = [0] * len(inputString)
+    i = 0
     for b in result:
         result[i] = ord(inputString[i]) ^ key
-        i=i+1
+        i += 1
     return result
 
-def calculateHammingDist( stringOne, stringTwo ):
-    "Hamming distance is the difference in bits of the two strings"
+
+def calculateHammingDist(stringOne, stringTwo):
+    'Hamming distance is the difference in bits of the two strings'
+
     hammingDist = 0
-    strOne = map( ord, stringOne )
-    strTwo = map( ord, stringTwo )
+    strOne = map(ord, stringOne)
+    strTwo = map(ord, stringTwo)
     # Ensure strings have the same length
-    if( len(stringOne) != len(stringTwo) ):
+    if(len(stringOne) != len(stringTwo)):
             return -1
 
     # Iterate through bytes, fine differing bits via XOR,
     # and increment hamming distance for each one.
-    for i in range( len(strOne) ):
+    for i in range(len(strOne)):
         b = strOne[i] ^ strTwo[i]
         for j in range(8):
-            if( b & 0x01 ):
-                hammingDist = hammingDist+1
+            if b & 0x01:
+                hammingDist += 1
             b = b >> 1
+
     return hammingDist
 
-def testKeysize( keySize, text ):
-    strOne = ""
-    strTwo = ""
-    strThree = ""
-    strFour = ""
+
+def testKeysize(keySize, text):
+    strings = ['', '', '', '']
+    
     for i in range(keySize):
-        strOne = strOne + text[i]
-        strTwo = strTwo + text[i+keySize]
-        strThree = strThree + text[i+(keySize*2)]
-        strFour = strFour + text[i+(keySize*3)]
-    hammDist1 = calculateHammingDist( strOne, strTwo )
-    hammDist2 = calculateHammingDist( strOne, strThree )
-    hammDist3 = calculateHammingDist( strOne, strFour )
-    hammDist4 = calculateHammingDist( strTwo, strThree )
-    hammDist5 = calculateHammingDist( strTwo, strFour )
-    hammDist6 = calculateHammingDist( strThree, strFour )
+        strings[0] = strings[0] + text[i]
+        strings[1] = strings[1] + text[i + keySize]
+        strings[2] = strings[2] + text[i + (keySize * 2)]
+        strings[3] = strings[3] + text[i + (keySize * 3)]
 
-    avgHammDist = float( hammDist1 + hammDist2 + hammDist3 + hammDist4 + hammDist5 + hammDist6 ) / float(6.0)
-    return float(avgHammDist) / float(keySize)
+    pairs = itertools.combinations(strings, 2)
+    hamming_distance = sum([calculateHammingDist(a, b) for (a, b) in pairs])
 
-def determineBlockKey( block ):
-    maxScore=0
-    maxScoreKey=0
-    for k in range(0,256):
-        text = decryptXORString( block, k )
-        score = alphaRangeScoreText( text )
-        if( maxScore < score ):
+    return hamming_distance * 1.0 / keySize
+
+
+def determineBlockKey(block):
+    maxScore = maxScoreKey = 0
+    
+    for k in range(0, 256):
+        text = decryptXORString(block, k)
+        score = alphaRangeScoreText(text)
+        if(maxScore < score):
             maxScore = score
             maxScoreKey = k
+
     return maxScoreKey
 
-def convertArrayToAscii( data ):
-    return ''.join(chr(i) for i in data )
+
+def convertArrayToAscii(data):
+    return ''.join(chr(i) for i in data)
 
 
 
 
 # Load Encrypted File
-print "Reading input file data."
-f = open("XOR-encrypted-file.txt", "r")
-ciphertext = f.read()
-f.close()
+print 'Reading input file data.'
+with open('XOR-encrypted-file.txt', 'r') as f:
+    ciphertext = f.read()
 
+    
 # Decode Base 64
-print "Base64 decoding ciphertext."
-ciphertext = base64.b64decode( ciphertext )
-
+print 'Base64 decoding ciphertext.'
+ciphertext = base64.b64decode(ciphertext)
+                              
 
 # Determine probable keysize via average hamming
-print "Calculating probable keysize..."
-minHamm=MAX_KEYSIZE * 8
+print 'Calculating probable keysize...'
+minHamm = MAX_KEYSIZE * 8
 keySize = 0
-for k in range(1,MAX_KEYSIZE):
-    avgHamm = testKeysize( k, ciphertext )
-    if( avgHamm < minHamm ):
+
+for k in range(1, MAX_KEYSIZE):
+    avgHamm = testKeysize(k, ciphertext)
+    if(avgHamm < minHamm):
         minHamm = avgHamm
         keySize = k
-print "\tLikely Keysize: " + str(keySize)
+print '\tLikely Keysize: ' + str(keySize)
+
 
 # Create text blocks via key size
-print "Determining Key"
-print "\tSplitting ciphertext into blocks"
-j=0
-cipher_text_block = [ "" for i in range(keySize) ]
+print 'Determining Key'
+print '\tSplitting ciphertext into blocks'
+j = 0
+cipher_text_block = [''] * keySize
 for k in ciphertext:
     cipher_text_block[j] = cipher_text_block[j] + k
-    j=j+1
+    j += 1
     if j >= keySize:
         j = 0
 
+        
 # Apply histograms to each to find most probably key characters
-print "\tCalculating likely key for block"
-block_key_value = ["" for i in range(keySize)]
-for b in range(len(cipher_text_block)):
-    block_key_value[b] = determineBlockKey( cipher_text_block[b] )
+print '\tCalculating likely key for block'
+block_key_value = [''] * keySize
 
-key=convertArrayToAscii(block_key_value)
-print "\tLikely key determined to be: \"" + key + "\""
+for b in range(len(cipher_text_block)):
+    block_key_value[b] = determineBlockKey(cipher_text_block[b])
+
+key = convertArrayToAscii(block_key_value)
+print '\tLikely key determined to be: \'' + key + '\''
 
 # Decrypt
-plaintext = encryptRepeatingXOR( ciphertext, key )
+plaintext = encryptRepeatingXOR(ciphertext, key)
 
-print "\n\n\n"
-print "***** BEGIN DECRYPTED TEXT *****"
-print "\n"
+print '\n\n\n'
+print '***** BEGIN DECRYPTED TEXT *****'
+print '\n'
 print convertArrayToAscii(plaintext)
-print "\n"
-print "***** END DECRYPTED TEXT *****"
-print "\n"
+print '\n'
+print '***** END DECRYPTED TEXT *****'
+print '\n'
